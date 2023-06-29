5be3da767df33b96073aa07d7d72d86a58cfba2e
Riyad <Riyad9876@gmail.com>
2023-06-29 10:16:00 -0400

    Loop Practice

---
+++ script.js
@@ -0,0 +1,46 @@
+// for (let i=6; i<=60;){
+//     console.log(i)
+//     i+=3
+
+// }
+
+// for (let i=0, st=''; i<=6;){
+//     st+='#'
+//     console.log(st)
+//     i+=1
+
+// }
+
+// for (let x=0,y=[], st=''; x<=20;x++){
+//     // x % 2 == 0 ? console.log(`${x}: Even`) : console.log(`${x}: Odd`);
+//     y.push(x)
+    // for (thing of y){
+    //     if (x == thing) {continue};
+    //     if (thing == 1) {continue};
+    //     if (x % thing == 0){
+    //         console.log("Prime")
+    //         break;
+    //     }
+    // }
+
+// }
+
+// let a = 0
+// y=[]
+// while (y.length<21){
+//     a+=1
+//     y.push(a)
+//     console.log(y);
+//     if (y.length == 20){
+
+//     }
+    // for (y[a-1]) {
+    //     if (a == thing) {continue};
+    //     if (thing == 1) {continue};
+    //     if ((a % thing == 0)){
+    //         console.log(a) 
+    //         console.log("Not Prime")
+    //         break;
+    // }
+    // }
+// }
